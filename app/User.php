33af7fe0d1102c3ed8d30cb95b8701cdce0c3aaf
Justin Tadlock <justin.b.tadlock@gmail.com>
2015-04-02 15:01:42 -0400

    Adding features to create some login functionality.

--- app/User.php
+++ app/User.php
@@ -15,14 +15,14 @@ class User extends Model implements AuthenticatableContract, CanResetPasswordCon
 	 *
 	 * @var string
 	 */
-	protected $table = 'user';
+	protected $table = 'UserLaravel';
 
 	/**
 	 * The attributes that are mass assignable.
 	 *
 	 * @var array
 	 */
-	protected $fillable = ['name', 'email', 'password'];
+	protected $fillable = ['username', 'email', 'password'];
 
 	/**
 	 * The attributes excluded from the model's JSON form.
@@ -34,11 +34,29 @@ class User extends Model implements AuthenticatableContract, CanResetPasswordCon
     /**
      * @var int Primary Key within the user table.
      */
-    protected $primaryKey = 'usr_id';
+    protected $primaryKey = 'usr_username';
     
     /**
      * @var string Database connection name.
      */
     protected $connection = 'myDB';
 
+    
+    /**
+     * Returns the user's username.
+     * 
+     * @return string
+     */
+    public function getUsernameAttribute() {
+        return $this->usr_username;
+    }
+    
+    /**
+     * Returns the user's encrypted password.
+     * 
+     * @return string
+     */
+    public function getPasswordAttribute() {
+        return $this->usr_password;
+    }
 }
