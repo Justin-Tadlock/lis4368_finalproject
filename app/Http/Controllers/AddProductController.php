0c5cf31254bea2c12585b13f83a74f81649ab546
Justin Tadlock <justin.b.tadlock@gmail.com>
2015-04-07 23:53:12 -0400

    Added PHP docs

--- app/Http/Controllers/AddProductController.php
+++ app/Http/Controllers/AddProductController.php
@@ -18,10 +18,21 @@ class AddProductController extends Controller {
         $this->middleware('auth');
     }
     
+    /**
+     * Load the AddProduct view
+     * 
+     * @return Response
+     */
 	public function getAddProduct() {
         return view('AddProduct');
     }
     
+    /**
+     * Validate and insert the Game data into the database.
+     * 
+     * @param ProductRequest $request
+     * @return Response
+     */
     public function postAddProduct(ProductRequest $request) {
         //Setting up the Game object to insert into the DB
         $this->game->setTitle     ($request->title);
