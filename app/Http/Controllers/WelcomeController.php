cbca28350c6a9ddc4f7a09af4019d9217cc39696
Justin Tadlock <justin.b.tadlock@gmail.com>
2015-04-07 21:30:33 -0400

    Fixed the endless redirect loop.

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -20,7 +20,7 @@ class WelcomeController extends Controller {
 	 */
 	public function __construct()
 	{
-		$this->middleware('guest');
+		//$this->middleware('guest'); //Removed to fix the endless loop error.
 	}
 
 	/**
